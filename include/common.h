6fc1bfadac0320a80f607f77a7d95fe25ecc174c
Justus Garbe <gihihoh@gmail.com>
2024-05-27 21:31:12 +0200

    feat: add unreachable for consteval utility

--- include/common.h
+++ include/common.h
@@ -31,3 +31,15 @@ constexpr u32 bswap32(u32 value) {
 }
 
 #define offsetof(type, member) __builtin_offsetof(type, member)
+
+[[noreturn]] inline void unreachable()
+{
+    // Uses compiler specific extensions if possible.
+    // Even if no extension is used, undefined behavior is still raised by
+    // an empty function body and the noreturn attribute.
+#if defined(_MSC_VER) && !defined(__clang__) // MSVC
+    __assume(false);
+#else // GCC, Clang
+    __builtin_unreachable();
+#endif
+}
